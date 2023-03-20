5616451136d8f1d250ccecc7508439c09e4e3043
yuma0313 <y.yuma0313@gmail.com>
2023-03-20 15:20:55 +0900

    微修正

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children, title = "Default title" }) => {
       </Head>
       <Navbar />
       <main>{children}</main>
-      <footer className="fixed bottom-0 left-0 w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+      <footer className="fixed bottom-0 left-0 w-full h-5 flex justify-center items-center text-gray-500 text-sm">
         @BlogApp
       </footer>
     </div>

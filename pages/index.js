5a599b080ab936b3e3a6e3b436e25f7725864d25
yuma0313 <y.yuma0313@gmail.com>
2023-04-16 20:21:27 +0900

    tailwindのレスポンシブ修正

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { client } from "../lib/client";
 export default function Home({ blog }) {
   return (
     <Layout title="ブログ一覧">
-      <div className="my-5 flex justify-center mx-5 min-h-[calc(100vh_-_100px)]">
+      <div className="my-5 flex flex-col md:flex-row justify-center mx-5 min-h-[calc(100vh_-_100px)]">
         <div className="p-4 my-3 bg-gray-50 w-3/5 mr-5">
           <ul>
             {blog.map((blog) => (
